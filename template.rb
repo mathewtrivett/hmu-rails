2ec89c4fd155ba48754ede630b89001794e29695
Mathew Trivett <mathew@wearesnook.com>
2020-10-09 18:14:25 +0100

    feat: simple rspec configuration setup
    @todo: prevent the generation of tests framework by default
    
    - Creates an .rspec file
    - Creates a spec directory
    - Creates a spec/support directory and adds support for factory_bot and shoulda-matchers
    - Adds simplecov, climate_control, rspec-rails, factory_bot and simplecov gems
    - Copies over a template .gitignore file
    - Configures spec_helper and rails_helper
    
    Manually tested but I wonder how to test rails templates / generators. I feel like each test run would take so long.

--- template.rb
+++ template.rb
@@ -34,7 +34,7 @@ end
 # Copies the template docs directory
 #
 def create_docs
-  directory "lib/templates/docs", "docs"
+  directory 'lib/templates/docs', 'docs'
 end
 
 ##
@@ -42,20 +42,57 @@ end
 # Overwrites the Rails default README
 #
 def create_readme
-  template "lib/templates/README.md.tt", "README.md", force: true
+  template 'lib/templates/README.md.tt', 'README.md', force: true
 end
 
 ##
 # Creates a boilerplate changelog
 #
 def create_changelog
-  template "lib/templates/CHANGELOG.md.tt", "CHANGELOG.md"
+  template 'lib/templates/CHANGELOG.md.tt', 'CHANGELOG.md'
+end
+
+def create_gitignore
+  template 'lib/templates/.gitignore', '.gitignore', force: true
+end
+
+def create_gemfile
+  template 'lib/templates/Gemfile.tt', 'Gemfile', force: true
+end
+
+##
+# Runs the rspec installer
+#
+def create_rspec
+  template 'lib/templates/.rspec', '.rspec'
+end
+
+def config_rspec
+  create_spec_helper
+  create_rails_helper
+end
+
+def create_spec_helper
+  template 'lib/templates/spec/spec_helper.rb.tt', 'spec/spec_helper.rb'
+end
+
+def create_rails_helper
+  template 'lib/templates/spec/rails_helper.rb', 'spec/rails_helper.rb'
+end
+
+def create_spec_support
+  directory 'lib/templates/spec/support', 'spec/support'
 end
 
 source_paths
+create_gitignore
+create_gemfile
 
 after_bundle do
   create_docs
   create_changelog
   create_readme
+  create_rspec
+  config_rspec
+  create_spec_support
 end
